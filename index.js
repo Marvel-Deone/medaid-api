29ecac913db976bd7a2fc95d86cf16571bd5e77f
Folagbade Olowofela <89777325+Marvel-Deone@users.noreply.github.com>
2023-04-02 17:05:57 +0100

    Edited the route page

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+})
+
 app.get('/', (req,res) =>{
     res.send("Medaid Project")
   
