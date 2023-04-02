de16dda5de2f7c8582c56b462e69e032fc39a4c3
Folagbade Olowofela <89777325+Marvel-Deone@users.noreply.github.com>
2023-04-02 16:02:41 +0100

    Edited the route page

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const AuthController = require('../controllers/AuthController')
 
-router.post('/register', AuthController.register)
-router.post('/login', AuthController.login)
+router.post('/auth/register', AuthController.register)
+router.post('/auth/login', AuthController.login)
 
 module.exports = router
