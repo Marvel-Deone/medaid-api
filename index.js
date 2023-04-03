186b701ab89913e89bb9c6540bcb54dd2779db45
Folagbade Olowofela <89777325+Marvel-Deone@users.noreply.github.com>
2023-04-03 10:43:56 +0100

    Updated cors code

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ app.use(cors({
     origin: '*'
 }));
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-// })
+
 
 app.get('/', (req,res) =>{
     res.send("Medaid Project")
