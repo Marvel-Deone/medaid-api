d0d5fdc9d739a78a61329bfd2d4ee22c1def96b9
Folagbade Olowofela <89777325+Marvel-Deone@users.noreply.github.com>
2023-05-17 18:01:02 +0100

    Updated

--- routes/medication.route.js
+++ routes/medication.route.js
@@ -7,6 +7,8 @@ const { verifyToken } = require('../middleware/validate_user_token')
 
 router.post('/', verifyToken, MedicationController.addMedication);
 router.get('/', verifyToken, MedicationController.getMedication);
+router.patch('/updateMedication/:id', verifyToken, MedicationController.updateMedication);
+router.delete('/deleteMedication/:id', verifyToken, MedicationController.deleteMedication);
 // router.get('/checkMedicationInterval', verifyToken, CheckMedicationIntervalController.checkMedicationInterval);
 
 module.exports = router
--- routes/reminder.route.js
+++ routes/reminder.route.js
@@ -6,5 +6,7 @@ const { verifyToken } = require('../middleware/validate_user_token')
 
 router.post('/', verifyToken, ReminderController.addReminder);
 router.get('/', verifyToken, ReminderController.getReminder);
+router.patch('/updateReminder/:id', verifyToken, ReminderController.updateReminder);
+router.delete('/deleteReminder/:id', verifyToken, ReminderController.deleteReminder);
 
 module.exports = router
